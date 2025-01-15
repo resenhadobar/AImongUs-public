61ee8c822fa268f7cb5a6422e8e4d83d0f09cdbd
resenhadobar <resenhadobar@gmail.com>
2025-01-15 12:43:56 -0300

    Deploy website - based on dfb41a58c3db528d876b89cad6b737202a1677db

--- packages/client-fxn/src/templates.ts
+++
@@ -1,9 +0,0 @@
-import {
-    messageCompletionFooter
-} from "@ai16z/eliza/src/parsing.ts";
-
-export const directorInitiationTemplate = `
-Bio: {{bio}}
-
-Task: Create a random, creative scene opening for a wrestling event. Format as "START: [scene description]". Make it unexpected but believable within wrestling context.
-` + messageCompletionFooter;
